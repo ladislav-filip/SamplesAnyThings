b81857e0c506922c2d948c03d824deb552909e6e
Ladislav Filip <ladislav.filip@gmail.com>
2022-11-04 21:55:00 +0100

    Zparcování různých událostí

--- WebSourceEvent/Program.cs
+++ WebSourceEvent/Program.cs
@@ -1,3 +1,5 @@
+using WebSourceEvent.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -21,5 +23,22 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+
+app.MapGet("/sse", async (ctx) =>
+{
+    var id = 0;
+    await ctx.SourceEventInitAsync();
+    var rnd = new Random();
+
+    var events = new[] { "message", "info" };
+    
+    while (true)
+    {
+        var evn = events[rnd.Next(2)];
+        Thread.Sleep((int)rnd.NextInt64(5000));
+        var tm = DateTime.Now.ToLongTimeString();
+        await ctx.SourceEventSendDataAsync(id++, evn, tm + ": " + HttpContextExpensions.GenerateName(10));
+    }
+});
 
 app.Run();
